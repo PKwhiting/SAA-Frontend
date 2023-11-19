3db60ba3f7cc2c6d66c3c587753e5dea0a4cd314
Patten Whiting <pattenwhiting@Pattens-MacBook-Pro.local>
2023-11-19 10:04:48 -0700

    updated about us

--- simple-web-app/src/views/AboutUs.vue
+++ simple-web-app/src/views/AboutUs.vue
@@ -4,7 +4,8 @@
       <div class="grid-1-columns gap-row-64px">
         <div
           id="w-node-afc06601-02ce-d8c0-5e67-f347696e7a1b-e9803f0d"
-          class="inner-container _384px _100-mbl" style="max-width: 100%"
+          class="inner-container _384px _100-mbl"
+          style="max-width: 100%"
         >
           <div
             data-current="Tab 1"
@@ -66,9 +67,14 @@
                     "
                     class="card-icon-square mg-bottom-16px"
                   /> -->
-                  <h3 class="text-200 bold">Our purpose</h3>
+                  <h3 class="text-200 bold">Our Purpose</h3>
                   <p class="mg-bottom-24px">
-                    The automotive world means everything to us. It gave us our first jobs and our first cars and we want to return the favor. The auto auction has become a world of scamming, expensive fees, and skethcy sellers. We aim to fix that. With transparency and honesty we hope to bring about a more honest marketplace one car at a time.
+                    The automotive world means everything to us. It gave us our
+                    first jobs and our first cars and we want to return the
+                    favor. The auto auction has become a world of scamming,
+                    expensive fees, and skethcy sellers. We aim to fix that.
+                    With transparency and honesty we hope to bring about a more
+                    honest marketplace one car at a time.
                   </p>
                   <!-- <a
                     data-w-id="e0b09591-1f95-fc6e-9ad3-43336eed97ed"
@@ -110,10 +116,14 @@
                     <div>
                       <h3 class="text-200 bold">Our Vision</h3>
                       <p class="mg-bottom-24px">
-                        Places like Copart and IAA have become difficult places to source vehicles. We aim to change that with out platform with
-                        more transparent and straightfoward pricing and fees. We want to make it easier for you to find and source the vehicles
-                        that you need whether you are looking for a new project car or purchasing vehicles for parts. We want to make it easier
-                        for you to find the vehicles that you need. 
+                        Places like Copart and IAA have become difficult places
+                        to source vehicles. We aim to change that with our
+                        platform with more transparent and straightfoward
+                        pricing and fees. We want to make it easier for you to
+                        find and source the vehicles that you need whether you
+                        are looking for a new project car or purchasing vehicles
+                        for parts. We want to make it easier for you to find the
+                        vehicles that you need.
                       </p>
                       <!-- <a
                         data-w-id="0975fc45-f5a8-822e-62f2-e213b7c55d3f"
@@ -155,9 +165,10 @@
                   <div class="pd-26px---24px---40px">
                     <h3 class="text-200 bold">Our Team</h3>
                     <p>
-                      Right now we are a small team trying to make a big difference. With only 
-                      a handful of employees we are the David to the Goliath that is the auto 
-                      auction world. We are Texas born and bred and carry the larger than life 
+                      Right now we are a small team trying to make a big
+                      difference. With only a handful of employees we are the
+                      David to the Goliath that is the auto auction world. We
+                      are Texas born and bred and carry the larger than life
                       philosophy into everything we do.
                     </p>
                     <!-- <a
@@ -183,21 +194,58 @@
       </div>
     </div>
     <div class="card component-card" style="margin-top: 24px">
-    <h2>Our Pricing</h2>
+      <h2>For Sellers</h2>
+      <div
+        id="w-node-_8c93b0bb-b935-c56a-dbd4-c83f1e999aeb-1e999aeb"
+        class="card pd-24px---18px---28px"
+        style="margin-bottom: 30px"
+      >
+      <div class="mg-bottom-4px">
+          <div class="text-600 bold color-neutral-800">$0</div>
+        </div>
+        <div class="mg-bottom-12px">No questions asked.</div>
+        <div class="grid-1-column gap-row-12px mg-bottom-24px">
+        <div class="mg-bottom-8px">
+          <div class="text-100 bold color-neutral-800">1. Submit your Vehicle Details:</div> Provide us with the necessary information about your vehicle(s) to get it up for auction.
+        </div>
+         <div class="mg-bottom-8px">
+          <div class="text-100 bold color-neutral-800">2. Set your Reserve Price:</div> Decide on the minimum price you're willing to accept, giving you control over the selling process.
+        </div>
+        <div class="mg-bottom-8px">
+          <div class="text-100 bold color-neutral-800">3. Auction Process:</div> Our platform will host an auction where potential buyers can place bids on your vehicle.
+        </div>
+        <div class="mg-bottom-8px">
+          <div class="text-100 bold color-neutral-800">4. Finalize the Sale:</div> Once the auction concludes, the highest bid meeting your reserve price leads to the sale, and we'll take care of the rest, ensuring a smooth transaction.
+        </div>
+
+        </div>
+        <!-- &lt;a
+            data-w-id=&quot;dc3b625c-4a68-4ebe-9b74-d3193fa9f32f&quot;
+            href=&quot;/&quot;
+            class=&quot;btn-primary w-inline-block&quot;
+            &gt;&lt;div class=&quot;flex-horizontal gap-column-4px&quot;&gt;
+              &lt;div&gt;Start Bidding&lt;/div&gt;
+              &lt;img
+                src=&quot;https://assets.website-files.com/645128e3dbdad55ed2803eff/646cdd3a1fe350c45874c7ce_primary-button-icon-right-dashflow-webflow-template.svg&quot;
+                loading=&quot;eager&quot;
+                altx=&quot;&quot;
+                class=&quot;link-icon arrow-right&quot;
+              /&gt;&lt;/div
+          &gt;&lt;/a&gt; -->
+      </div>
+      <h2>For Buyers</h2>
       <div class="grid-2-columns _1-col-tablet">
         <div
           id="w-node-_8c93b0bb-b935-c56a-dbd4-c83f1e999aeb-1e999aeb"
           class="card pd-24px---18px---28px"
         >
           <div class="mg-bottom-8px">
-            <div class="text-100 bold color-neutral-800">Vehicle Purchases</div>
+            <div class="text-100 bold color-neutral-800">Private Buyers</div>
           </div>
           <div class="mg-bottom-4px">
-            <div class="text-600 bold color-neutral-800">$200/Vehicle</div>
+            <div class="text-600 bold color-neutral-800">3% of Purchase Price</div>
           </div>
-          <div class="mg-bottom-12px">
-            Simple as that.
-          </div>
+          <div class="mg-bottom-12px">Max of $800 </div>
           <div class="grid-1-column gap-row-12px mg-bottom-24px">
             <div
               id="w-node-_8c93b0bb-b935-c56a-dbd4-c83f1e999af5-1e999aeb"
@@ -210,19 +258,6 @@
               />
               <div class="text-100 medium color-neutral-700">
                 No pickup scheduling
-              </div>
-            </div>
-            <div
-              id="w-node-_8c93b0bb-b935-c56a-dbd4-c83f1e999af9-1e999aeb"
-              class="flex align-start gap-column-6px"
-            >
-              <img
-                src="https://assets.website-files.com/645128e3dbdad55ed2803eff/6453fdd7348b8a173bec08e2_feature-icon-left-dashflow-webflow-template.svg"
-                loading="eager"
-                alt=""
-              />
-              <div class="text-100 medium color-neutral-700">
-                No percentage of sale.
               </div>
             </div>
             <div
@@ -247,9 +282,7 @@
                 loading="eager"
                 alt=""
               />
-              <div class="text-100 medium color-neutral-700">
-                No gate fees.
-              </div>
+              <div class="text-100 medium color-neutral-700">No gate fees.</div>
             </div>
             <div
               id="w-node-_8c93b0bb-b935-c56a-dbd4-c83f1e999b05-1e999aeb"
@@ -265,7 +298,7 @@
               </div>
             </div>
           </div>
-          <a
+          <!-- <a
             data-w-id="dc3b625c-4a68-4ebe-9b74-d3193fa9f32f"
             href="/"
             class="btn-primary w-inline-block"
@@ -277,21 +310,19 @@
                 alt=""
                 class="link-icon arrow-right"
               /></div
-          ></a>
+          ></a> -->
         </div>
-         <div
+        <div
           id="w-node-_8c93b0bb-b935-c56a-dbd4-c83f1e999aeb-1e999aeb"
           class="card pd-24px---18px---28px"
         >
           <div class="mg-bottom-8px">
-            <div class="text-100 bold color-neutral-800">Auctioning Off</div>
+            <div class="text-100 bold color-neutral-800">Commercial Buyer</div>
           </div>
           <div class="mg-bottom-4px">
-            <div class="text-600 bold color-neutral-800">$0</div>
+            <div class="text-600 bold color-neutral-800">$300 Per Vehicle</div>
           </div>
-          <div class="mg-bottom-12px">
-            No questions asked.
-          </div>
+          <div class="mg-bottom-12px">Simple as that.</div>
           <div class="grid-1-column gap-row-12px mg-bottom-24px">
             <div
               id="w-node-_8c93b0bb-b935-c56a-dbd4-c83f1e999af5-1e999aeb"
@@ -319,19 +350,7 @@
                 Simplified paperwork.
               </div>
             </div>
-            <div
-              id="w-node-_8c93b0bb-b935-c56a-dbd4-c83f1e999afd-1e999aeb"
-              class="flex align-start gap-column-6px"
-            >
-              <img
-                src="https://assets.website-files.com/645128e3dbdad55ed2803eff/6453fdd7348b8a173bec08e2_feature-icon-left-dashflow-webflow-template.svg"
-                loading="eager"
-                alt=""
-              />
-              <div class="text-100 medium color-neutral-700">
-                Easy listing.
-              </div>
-            </div>
+
             <div
               id="w-node-_8c93b0bb-b935-c56a-dbd4-c83f1e999b01-1e999aeb"
               class="flex align-start gap-column-6px"
@@ -355,7 +374,7 @@
                 alt=""
               />
               <div class="text-100 medium color-neutral-700">
-                Faster selling.
+                Faster buying.
               </div>
             </div>
           </div>
@@ -488,6 +507,22 @@
             ></a>
           </div>
         </div> -->
+      </div>
+      <div>
+        <a
+            data-w-id="dc3b625c-4a68-4ebe-9b74-d3193fa9f32f"
+            href="/"
+            class="btn-primary w-inline-block"
+            style="display: flex; justify-content: center; align-items: center; margin-top:40px"
+            ><div class="flex-horizontal gap-column-4px">
+              <div>Start Bidding</div>
+              <img
+                src="https://assets.website-files.com/645128e3dbdad55ed2803eff/646cdd3a1fe350c45874c7ce_primary-button-icon-right-dashflow-webflow-template.svg"
+                loading="eager"
+                alt=""
+                class="link-icon arrow-right"
+              /></div
+          ></a>
       </div>
     </div>
   </div>
